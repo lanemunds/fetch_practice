9493027121aec34528c71a16c6728c696403959e
lanemunds <lanemunds@gmail.com>
2023-03-26 21:36:56 -0600

    added second

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function fetchData(){
-    d =[]
 fetch('https://reqres.in/api/users').then(response =>{
     return response.json()
 }).then(data =>{
-    console.log(data.data)
     const html = data.data.map(user=>{
         return `<div class = "user">
         <p>Name: ${user.first_name} ${user.last_name} </p>
@@ -19,3 +17,71 @@ fetch('https://reqres.in/api/users').then(response =>{
 
 }
 fetchData()
+
+function fetchStar(){
+    d=[]
+    fetch('https://swapi.dev/api/planets/1').then(response=>{
+        return response.json()
+    }).then(data=>{
+     let html = `<div>
+        <p>Name:${data.name}</p>
+        <p>Climate:${data.climate}</p>
+     </div>`
+     document.querySelector('#starwars').insertAdjacentHTML('afterbegin',html)
+     
+     document.querySelector('#starwars').insertAdjacentHTML('beforebegin','<h1>Residents</h1>')
+     for(let i = 0;i < data.residents.length; i++){
+         fetch(data.residents[i]).then(response=>{
+             return response.json()
+            }).then(data=>{
+                let html1 = `
+                <div>
+                <p>
+                ${data.name}
+                </p>
+                </div>
+                `
+                document.querySelector('#starwars').insertAdjacentHTML('afterbegin',html1)
+                
+            })
+        }
+    })
+    
+
+}
+
+fetchStar()
+
+
+function fetchStar1(){
+    fetch('https://swapi.dev/api/planets/2').then(response=>{
+        return response.json()
+    }).then(data=>{
+        let html = `<div>
+        <p>${data.name}</p>
+        </div>`
+        document.querySelector('#starwars').insertAdjacentHTML('beforeend',html)
+    })
+}
+
+fetchStar1()
+
+
+function writeHello(){
+    word = `<div>
+    <p>Hello</p>
+    </div>`
+    document.querySelector('#hello').insertAdjacentHTML('beforebegin', word)
+}
+
+writeHello()
+
+function writeSomething(sentence){
+
+    sent=`<div>
+    ${sentence}
+    </div>`
+document.querySelector('#tricks').insertAdjacentHTML('beforebegin',sent)
+}
+
+writeSomething('tricks are for kids')
